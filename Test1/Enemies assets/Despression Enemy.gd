fd5c7789493d82f3768b8939fb20a44a53aa75eb
ZetaSense <zetasense2@gmail.com>
2024-04-13 15:37:07 -0400

    PUSH TAKE 3!!! Got enemy code from yt tutorial. Going to personalize it next.

---
+++ Test1/Enemies assets/Despression Enemy.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+const WALK_SPEED = 75
+const GRAVITY = 600
+const JUMP_VELOCITY = -400
+
+@export var health = 1
+onready var player = get.node("/root/World/Students")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if player:
+		#Get direction (Vector2) from current enemy's position to player's postition
+		var direction = (player.position - postion).normalized()
+		if not is_on_floor():
+			direction.y += GRAVITY
+		
+		move_and_slide(direction - WALK_SPEED)
+		
+		for i in get_slide_count():
+			var collection = get_slide_collision(i)
+			if collsion.collider.name == "Students":
+				print("dead")
+
