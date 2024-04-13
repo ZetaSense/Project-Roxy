3b12be7121c6e4e74ef38584958713d0b78622ce
MikeNaz101 <mike.yamaha.naz@gmail.com>
2024-04-13 16:29:37 -0400

    Student and basic enemies
    the enemies is just a place holder for seths baddies so i can think about the damage...

---
+++ Test1/student.gd
@@ -0,0 +1,15 @@
+# Student.gd
+extends Area2D
+
+
+var health = 100
+var DAMAGE_AMOUNT = 10
+
+# If Enemy touches the student
+func _on_area_entered(area):
+	health -= DAMAGE_AMOUNT
+	if health <= 0:
+		health = 0
+		# Game over logic here 
+	# Enemy will disappear here
+	update_health_ui()
