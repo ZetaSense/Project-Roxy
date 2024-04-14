56365024b06cba1af0afa61760d32d57aedd0431
MikeNaz101 <mike.yamaha.naz@gmail.com>
2024-04-14 01:13:12 -0400

    Student and HealthBar
    made the students healthbar show and (hopefully) change when attacked my enemy

--- Test1/MHealthBar.gd
+++ Test1/MHealthBar.gd
@@ -1,10 +1,37 @@
 extends ProgressBar
 
 @onready var student = get_parent()
+@onready var timer = $Timer
+@onready var damage_bar = $DamageBar
 
-# Called when the node enters the scene tree for the first time.
+var health = 0 : set = _set_health
 
+func _set_health(new_health):
+	var prev_health = health
+	health = min(max_value, new_health)
+	value = health
+	
+	if health <= 0:
+		queue_free()
+		
+	if health < prev_health:
+		timer.start()
+	else:
+		damage_bar.value = health
+		
+	
+func _init_health(_health):
+	health = _health
+	max_value = health
+	value = health
+	damage_bar.max_value = health
+	damage_bar.value = health
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update():
 	value = student.currentHealth * 100 / student.maxHealth
+
+
+func _on_timer_timeout():
+	damage_bar.value = health
--- Test1/student.gd
+++ Test1/student.gd
@@ -1,22 +1,31 @@
 # Student.gd
 extends Area2D
-
 class_name Student
 
-signal healthChanged
+signal isDamaged
 
+@onready var health
+@onready var healthBar = $MHealthBar
 @export var maxHealth = 100
 @onready var currentHealth = maxHealth
 var DAMAGE_AMOUNT = 10
+
+func _ready():
+	health = maxHealth
+	healthBar._init_health(health)
+	
+
+
 
 # If Enemy touches the student
 func _on_area_entered(area):
 	if area.is_in_group("enemy"):
 		currentHealth -= DAMAGE_AMOUNT
+		healthBar._set_health(currentHealth)
 		if currentHealth <= 0:
 			currentHealth = 0
-	healthChanged.emit()
-	$MHealthBar.update()
+			isDamaged.emit()
+	# $MHealthBar.update()
 		# Game over logic here 
 	# Enemy will disappear here
 	
